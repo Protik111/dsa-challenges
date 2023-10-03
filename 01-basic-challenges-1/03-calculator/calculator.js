9ef46e569d93dce3a728104d3b49e71f8f73cdad
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-03 20:57:39 +0600

    count occurences added

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,20 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+      break;
+    case "-":
+      return num1 - num2;
+      break;
+    case "*":
+      return num1 * num2;
+      break;
+    case "/":
+      return num1 / num2;
+      break;
+    default:
+      throw new Error("Invalid operator");
+  }
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,28 @@
-function countOccurrences() {}
+//Solution 1
+// function countOccurrences(word, letter) {
+//   let count = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] == letter) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+//Solution 2
+// function countOccurrences(word, letter) {
+//   const wordArr = word.split("");
+//   let count = 0;
+//   wordArr.forEach((element) => {
+//     if (element === letter) {
+//       count++;
+//     }
+//   });
+
+//   return count;
+// }
+
+//Solution 3
+const countOccurrences = (str, char) => str.split(char).length - 1;
 
 module.exports = countOccurrences;

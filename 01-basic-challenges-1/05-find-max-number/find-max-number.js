bfe06f39ca75629dc787bce0733d160a9dd5568e
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-04 08:53:02 +0600

    findMax added

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,16 @@
-function findMaxNumber() {}
+function findMaxNumber(numberArr) {
+    let largestNumber = 0;
+    for (let i = 0; i < numberArr.length; i++) {
+        if (numberArr[i] > largestNumber) {
+            largestNumber = numberArr[i]
+        }
+    }
+    return largestNumber;
+}
+
+//Solution 2
+// function findMaxNumber(numberArr) {
+//     return Math.max(...numberArr)
+// }
 
 module.exports = findMaxNumber;

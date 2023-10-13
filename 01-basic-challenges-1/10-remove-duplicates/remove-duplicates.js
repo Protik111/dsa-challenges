1fb190542bc1b857b38c6172ac12e4429b0832f7
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-14 00:10:51 +0600

    remove dupliates added

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+function removeDuplicates(duplicatesArr) {
+  const set = new Set();
+  duplicatesArr.forEach((element) => {
+    if (!set.has(element)) {
+      set.add(element);
+    }
+  });
+  return [...set];
+}
+
+//Solution 3
+// function removeDuplicates(arr) {
+//   return Array.from(new Set(arr));
+// }
 
 module.exports = removeDuplicates;

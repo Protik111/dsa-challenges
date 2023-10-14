3c9bd0bee09840204c1f387a7e4cdb402a2294a1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-14 19:49:33 +0600

    FizzBuzz array  added

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array-run.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array-run.js
@@ -1,5 +1,7 @@
-const fizzBuzzArray = require('./fizzbuzz-array');
+const fizzBuzzArray = require("./fizzbuzz-array");
 
 const result = fizzBuzzArray(15);
+// const result2 = fizzBuzzArray(5);
 
 console.log(result);
+// console.log(result2);
--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(number) {
+  let newArr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      newArr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      newArr.push("Fizz");
+    } else if (i % 5 === 0) {
+      newArr.push("Buzz");
+    } else {
+      newArr.push(i);
+    }
+  }
+
+  return newArr;
+}
 
 module.exports = fizzBuzzArray;

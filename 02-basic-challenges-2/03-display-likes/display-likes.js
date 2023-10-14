eefbb7daca4736fc89cf7b1afe63195d906d52f1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-14 22:27:39 +0600

    likes solution added

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,26 @@
-function displayLikes() {}
+function displayLikes(personArr) {
+  if (!Array.isArray(personArr)) {
+    return false;
+  }
+
+  const length = personArr.length;
+  let text = "";
+
+  if (personArr.length === 0) {
+    text = "no one likes this";
+  } else if (personArr.length === 1) {
+    text = `${personArr[0]} likes this`;
+  } else if (personArr.length === 2) {
+    text = `${personArr[0]} and ${personArr[1]} like this`;
+  } else if (personArr.length === 3) {
+    text = `${personArr[0]}, ${personArr[1]} and ${personArr[2]} like this`;
+  } else if (personArr.length >= 3) {
+    text = `${personArr[0]}, ${personArr[1]} and ${
+      length - 2
+    } others like this`;
+  }
+
+  return text;
+}
 
 module.exports = displayLikes;

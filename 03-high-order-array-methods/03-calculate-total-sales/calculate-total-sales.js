9d870498f826438875d3b6cf0114a41491caab10
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-02 16:24:33 +0600

    [DONE] calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  const totalPrice = products.reduce((sum, product) => {
+    return sum + product.price * product.quantity;
+  }, 0);
+
+  const taxAmount = (totalPrice * tax) / 100;
+  const priceWithTax = taxAmount + totalPrice;
+  // return parseFloat(priceWithTax.toFixed(2));
+  return Number(priceWithTax);
+}
 
 module.exports = calculateTotalSalesWithTax;

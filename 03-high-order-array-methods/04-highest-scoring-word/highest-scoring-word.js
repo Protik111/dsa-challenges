80c5d828229762448daed298cf756711ad0604f9
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-02 16:57:58 +0600

    [DONE] highest scroring word soltions

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,17 @@
-function highestScoringWord() {}
+function highestScoringWord(sentence) {
+  const words = sentence.split(" ");
+
+  const wordsIntoVal = words.map((word) =>
+    Array.from(word).reduce(
+      (score, letter) => score + letter.charCodeAt(0) - 96,
+      0
+    )
+  );
+
+  const highestScore = Math.max(...wordsIntoVal);
+  const indexOfHighestScore = wordsIntoVal.indexOf(highestScore);
+
+  return words[indexOfHighestScore];
+}
 
 module.exports = highestScoringWord;

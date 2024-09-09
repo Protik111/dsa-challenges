5b63de56fd064eaa2e3668a3611b2332511f510e
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-09 17:31:25 +0600

    [DONE] valid ip check

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
@@ -1,6 +1,14 @@
-const isValidIPv4 = require('./valid-ipv4.js');
+const isValidIPv4 = require("./valid-ipv4.js");
 
-const result1 = isValidIPv4('122.164.23.21');
-const result2 = isValidIPv4('122.164.23.21.33');
+const result1 = isValidIPv4("122.164.23.21");
+const result2 = isValidIPv4("122.164.23.21.33");
 
-console.log(result1, result2);
+console.log(isValidIPv4("1.2.3.4")); // true
+console.log(isValidIPv4("123.45.67.89")); // true
+console.log(isValidIPv4("1.2.3")); // false
+console.log(isValidIPv4("1.2.3.4.5")); // false
+console.log(isValidIPv4("123.456.78.90")); // false
+console.log(isValidIPv4("123.045.067.089")); // false
+
+console.log(result1);
+console.log(result2);
--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,13 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+  const spiltedIp = ip.split(".");
+
+  if (spiltedIp?.length !== 4) return false;
+
+  return spiltedIp.every((octate) => {
+    const num = Number(octate);
+
+    return num >= 0 && num <= 255 && String(num) === octate;
+  });
+};
 
 module.exports = isValidIPv4;

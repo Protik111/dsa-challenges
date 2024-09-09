c2bb2a20dbba1938c9af9e09b9e93343c74c0bb5
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-09 16:25:05 +0600

    [DONE] hashtag generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,19 @@
-function generateHashtag() {}
+function capitalizeWord(word) {
+  const capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1);
+  return capitalizedWord;
+}
+
+function generateHashtag(str) {
+  if (str.trim() === "" || str.length >= 140) {
+    return false;
+  }
+  const strArr = str.split(" ");
+  let hashed = "";
+  strArr.forEach((element) => {
+    const capitalizedWord = capitalizeWord(element);
+    hashed = hashed + capitalizedWord;
+  });
+  return `#${hashed}`;
+}
 
 module.exports = generateHashtag;

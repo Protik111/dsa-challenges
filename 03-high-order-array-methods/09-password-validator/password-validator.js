c86a76d49912603852afbc8adb5965f7c9f05c9b
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-10 13:38:03 +0600

    [DONE] password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,11 @@
-function validatePassword() {}
+function validatePassword(password) {
+  if (password?.length < 8) return false;
+
+  const hasUppercase = [...password].some((char) => char >= "A" && char <= "Z");
+  const hasLowercase = [...password].some((char) => char >= "a" && char <= "z");
+  const hasDigit = [...password].some((char) => char >= "0" && char <= "9");
+
+  return hasUppercase && hasLowercase && hasDigit;
+}
 
 module.exports = validatePassword;

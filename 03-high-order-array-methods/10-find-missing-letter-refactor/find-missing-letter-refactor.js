a22eb944e0d4ce2a02c7811ba29e7285a733df7b
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-10 17:07:40 +0600

    [DONE] find missing lette with higher order method

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,23 @@
-function findMissingLetter() {}
+function findMissingLetter(letters) {
+  if (letters?.letters < 2) return false;
+
+  const uniCodes = letters.map((char) => char.charCodeAt());
+
+  let start = letters[0].charCodeAt(0);
+
+  const missingCode = uniCodes.find((uniCode) => {
+    if (uniCode - start > 1) {
+      return true;
+    }
+    start = uniCode;
+    return false;
+  });
+
+  if (missingCode) {
+    return String.fromCharCode(start + 1);
+  }
+
+  return null;
+}
 
 module.exports = findMissingLetter;

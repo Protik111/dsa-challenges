ff0168ee903cb4935738ec9e90a82a7d0c89dce9
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-10 13:01:31 +0600

    [FIX] analyze card mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,25 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((total, car) => {
+    return total + car.mileage;
+  }, 0);
+  const averageMileage = totalMileage / cars?.length;
+
+  const highestMileageCar = cars.reduce((highestCar, currentCar) => {
+    return currentCar?.mileage > highestCar?.mileage ? currentCar : highestCar;
+  }, cars[0]);
+
+  const lowestMileageCar = cars.reduce((lowestMilageCar, currentCar) => {
+    return currentCar?.mileage < lowestMilageCar?.mileage
+      ? currentCar
+      : lowestMilageCar;
+  }, cars[0]);
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage: Number(totalMileage),
+  };
+}
 
 module.exports = analyzeCarMileage;

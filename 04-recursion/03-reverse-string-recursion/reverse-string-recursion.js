02b61f8fa825c02957597eab1ba0ad604252ed30
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-18 17:56:13 +0600

    [DONE] reverse string with recursive func

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else {
+    // Recursive case - return the last character of str and call reverseString again
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+}
 
 module.exports = reverseString;

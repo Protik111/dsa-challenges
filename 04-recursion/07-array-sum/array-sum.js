db1ba595b17c16cb01928bfd1126a1168c549924
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-28 16:34:55 +0600

    [DONE] array some with recursion

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,9 @@
-function arraySum() {}
+function arraySum(arrNum) {
+  if (arrNum?.length === 0) {
+    return 0;
+  }
+
+  return arrNum[0] + arraySum(arrNum.slice(1));
+}
 
 module.exports = arraySum;

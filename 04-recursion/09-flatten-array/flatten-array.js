4908ffe9557a4daaa1eadfb277376422cf16216b
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-09-28 17:54:27 +0600

    [DONE] flatten array with recursion

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,15 @@
-function flattenArray() {}
+function flattenArray(arrNumbers) {
+  let numbers = [];
+
+  for (const item of arrNumbers) {
+    if (Array.isArray(item)) {
+      numbers = numbers.concat(flattenArray(item));
+    } else {
+      numbers.push(item);
+    }
+  }
+
+  return numbers;
+}
 
 module.exports = flattenArray;

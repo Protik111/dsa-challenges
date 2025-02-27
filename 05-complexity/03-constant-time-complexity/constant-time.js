2f5f7188937e0f63c12709e1e94099a07b1c8254
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-02-27 16:48:14 +0600

    time difference in constant-complexity

--- 05-complexity/03-constant-time-complexity/constant-time.js
+++ 05-complexity/03-constant-time-complexity/constant-time.js
@@ -3,3 +3,18 @@
 
   Constant time means that the time required to complete a function is the same regardless of the size of the input data set.
 */
+
+const arr = [1, 2, 3, 4, 5];
+const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1);
+
+function findIndexValue(arr, index) {
+  return arr[index];
+}
+
+const start1 = performance.now();
+console.log(findIndexValue(arr, 1));
+console.log("Time 1", performance.now() - start1);
+
+const start2 = performance.now();
+console.log(findIndexValue(arr2, 1));
+console.log("Time 2", performance.now() - start2);

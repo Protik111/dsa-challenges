918ea27957b7120097311da9c238910a0487186d
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-04-08 12:50:03 +0600

    [ADDED] max sub array with quadratic

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,17 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = 0;
+
+  for (let i = 0; i <= arr.length - k; i++) {
+    let sumOfArr = 0;
+
+    for (let j = i; j < i + k; j++) {
+      sumOfArr += arr[j];
+    }
+
+    maxSum = Math.max(maxSum, sumOfArr);
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;

84b0689413fc13f65fbc53eeeebf08aeb7bc04f3
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-04-08 16:49:02 +0600

    [ADDED] max sub array with linear time complexity

---
+++ 05-complexity/09-sliding-window-technique/max-subarray-linear-solution.js
@@ -0,0 +1,23 @@
+function maxSubarraySum(arr, k) {
+  if (arr.length < k) return null;
+
+  // Step 1: Calculate the sum of the first window
+  let windowSum = 0;
+  for (let i = 0; i < k; i++) {
+    windowSum += arr[i];
+  }
+
+  let maxSum = windowSum;
+
+  // Step 2: Slide the window forward
+  for (let i = k; i < arr.length; i++) {
+    // Slide the window by subtracting the element that is left behind
+    // and adding the new one
+    windowSum = windowSum - arr[i - k] + arr[i];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum;
+}
+
+[2, 1, 5, 1, 3, 2];

93ace042b5a4f38e877674accdbcf24924930a58
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-05-27 11:15:57 +0600

    feat: word frequency with map

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,22 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  if (str.length === 0) return {};
+
+  const words = str.toLowerCase().split(/\W+/);
+
+  const wordMap = new Map();
+
+  for (const word of words) {
+    if (word === "") continue;
+
+    if (wordMap.get(word)) {
+      wordMap.set(word, wordMap.get(word) + 1);
+    } else {
+      wordMap.set(word, 1);
+    }
+  }
+  return wordMap;
+}
+
+// map --> {the: 3, man: 1}
 
 module.exports = wordFrequencyCounter;

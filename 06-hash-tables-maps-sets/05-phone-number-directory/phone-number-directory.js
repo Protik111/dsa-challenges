c3e0c380550780962f0184a8485c815949c1f197
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-05-28 10:04:00 +0600

    feat: phone number dictionary

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(strArr) {
+  const dictionary = new Map();
+
+  strArr.forEach((str) => {
+    const [name, phone] = str.split(":");
+    dictionary.set(name, phone);
+  });
+
+  return dictionary;
+}
 
 module.exports = phoneNumberDirectory;

43f4267a618106c8b303353f50d5c7a8903fdf80
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-06-02 13:19:59 +0600

    feat: symmetric difference added

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,22 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const symmetricArr = [];
+
+  for (const num of arr1) {
+    if (!set2.has(num)) {
+      symmetricArr.push(num);
+    }
+  }
+
+  for (const num of arr2) {
+    if (!set1.has(num)) {
+      symmetricArr.push(num);
+    }
+  }
+
+  return symmetricArr;
+}
 
 module.exports = symmetricDifference;

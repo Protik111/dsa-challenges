ce878b2d363e473ea0f56ded9c8e78a12ccd335a
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-06-02 13:19:40 +0600

    feat: angram added

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,19 @@
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  if (words?.length === 0) return;
+
+  const anagrams = new Map();
+
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+
+    if (anagrams.has(sortedWord)) {
+      anagrams.get(sortedWord).push(word);
+    } else {
+      anagrams.set(sortedWord, [word]);
+    }
+  }
+
+  return Array.from(anagrams.values());
+}
 
 module.exports = anagramGrouping;

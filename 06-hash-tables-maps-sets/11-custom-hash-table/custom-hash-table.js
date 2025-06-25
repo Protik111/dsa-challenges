814b947998cd26b7f8e2fcac77b6af5340863c8c
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-06-25 09:54:10 +0600

    feat: hash table hash function

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,18 @@
-class HashTable {}
+class HashTable {
+  constructor(limit = 14) {
+    this.storage = [];
+    this.limit = limit;
+  }
+
+  //hash function
+  _hash(key, max) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+
+    return hash % max;
+  }
+}
 
 module.exports = HashTable;

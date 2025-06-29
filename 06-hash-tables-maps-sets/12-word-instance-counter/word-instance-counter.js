07303b7ae79805dc6a88f38811d33109953beb29
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-06-29 10:37:05 +0600

    feat: word-instance-counter with hash table

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,27 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+  const words = str.toLowerCase().split(/\w+/);
+  const wordFrequency = new HashTable();
+  const targetWord = word.toLowerCase();
+  const count = 0;
+
+  for (const currentWord of words) {
+    //continue if the word is empty string
+    if (currentWord === "") continue;
+
+    if (wordFrequency.has(currentWord)) {
+      wordFrequency.set(currentWord, wordFrequency.get(currentWord) + 1);
+    } else {
+      wordFrequency.set(currentWord, 1);
+    }
+
+    if (currentWord === targetWord) {
+      count = wordFrequency.get(currentWord);
+    }
+  }
+
+  return count;
+}
 
 module.exports = wordInstanceCounter;

8549e5c542082196123d0a7212aeb07fef85706d
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-08 11:23:34 +0600

    feat: custom anagram grouping

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,19 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const anagramGroups = new HashTable();
+
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+
+    if (anagramGroups.has(sortedWord)) {
+      anagramGroups.get(anagramGroups).push(word);
+    } else {
+      anagramGroups.set(sortedWord, [word]);
+    }
+  }
+
+  return anagramGroups.getValues();
+}
 
 module.exports = anagramGrouping;

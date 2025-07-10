f7b50f864d8e215bbe251fa934aad7ec3d77c561
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-10 11:06:23 +0600

    feat: stack custom push method

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,28 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.maxSize = 100;
+    this.stack = [];
+    this.top = -1;
+  }
+
+  push(value) {
+    if (this.isFull()) {
+      return false;
+    }
+
+    this.top++;
+
+    this.stack[this.top] = value;
+    return true;
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+}
 
 module.exports = Stack;

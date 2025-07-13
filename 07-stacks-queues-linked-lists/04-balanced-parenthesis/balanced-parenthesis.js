287650e6da35905291b4674d2d8d3b9704a51f17
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-13 17:14:23 +0600

    feat: balanced parenthesis with stack

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,21 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-function balancedParenthesis() {}
+function balancedParenthesis(parenthesisStr) {
+  const stack = new Stack();
+
+  for (let i = 0; i < parenthesisStr.length; i++) {
+    if (parenthesisStr[i] === "(") {
+      stack.push(parenthesisStr[i]);
+    } else if (parenthesisStr[i] === ")") {
+      if (stack.isEmpty()) {
+        return false;
+      }
+
+      stack.pop();
+    }
+  }
+
+  return stack.isEmpty();
+}
 
 module.exports = balancedParenthesis;

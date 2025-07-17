6b7831bbbc8a07ecde417009674d1a4b2a05bcd9
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-17 10:58:47 +0600

    feat: palindrome with stack and queue

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,24 @@
-const Queue = require('./queue');
-const Stack = require('./stack');
+const Queue = require("./queue");
+const Stack = require("./stack");
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+  const queue = new Queue();
+  const stack = new Stack();
+
+  for (let i = 0; i < str.length; i++) {
+    queue.enqueue(str[i]);
+    stack.push(str[i]);
+  }
+
+  while (!queue.isEmpty()) {
+    const queueChar = queue.dequeue();
+    const stackChar = stack.pop();
+    if (queueChar !== stackChar) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = isPalindromeQueueStack;

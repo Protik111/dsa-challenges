bde578ea8e0ca319eaba14c3939a84084c54f849
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-21 11:16:10 +0600

    feat: linked list class with add method

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -1,5 +1,27 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node; // We’ve linked the new node, but tail still points to 20
+    }
+
+    this.tail = node; // Now tail points to the correct last node.
+  }
+}
 
 module.exports = { Node, LinkedList };

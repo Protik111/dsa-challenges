489416335e0e2ee6ab220b7acfe75840b134dc02
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-07-29 11:27:47 +0600

    feat: fast and slow pointer pattern

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,20 @@
-function findMiddle() {}
+function findMiddle(list) {
+  let fast = list.head;
+  let slow = list.head;
+  let prev = null;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    prev = slow;
+    slow = slow.next;
+  }
+
+  if (fast === null) {
+    //return for even
+    return prev.next;
+  } else {
+    return slow;
+  }
+}
 
 module.exports = findMiddle;

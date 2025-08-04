4e3da843ba09e5c8690f766075cfddffa3f0cff1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-08-04 11:08:13 +0600

    feat: find middle flow with excalidraw

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -4,6 +4,7 @@ function findMiddle(list) {
   let prev = null;
 
   while (fast !== null && fast.next !== null) {
+    // this loops continues as long as there are at least two nodes for fast to move
     fast = fast.next.next;
     prev = slow;
     slow = slow.next;

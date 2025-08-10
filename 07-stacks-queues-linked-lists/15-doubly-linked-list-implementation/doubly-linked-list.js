e3266cf8fad3bf8488bcb546b5899d18eb409d4e
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-08-10 11:22:55 +0600

    feat: doubly linked list append method

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list.js
@@ -1,5 +1,31 @@
-function Node() {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
 
-function DoublyLinkedList() {}
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = this.tail = newNode; //newNode = { data: 10, next: null, prev: null }
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+}
 
 module.exports = DoublyLinkedList;

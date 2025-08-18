ec9d427b6af31852b68df72fe786ea78fe7c22c8
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-08-18 10:58:59 +0600

    feat: find pair some class

--- 07-stacks-queues-linked-lists/16-find-pair-sum/DoublyLinkedList.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/DoublyLinkedList.js
@@ -1,143 +1,119 @@
-function Node(data) {
-  this.data = data;
-  this.next = null;
-  this.prev = null;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
 }
 
-function DoublyLinkedList() {
-  this.head = null;
-  this.tail = null;
-  this.length = 0;
-}
-
-DoublyLinkedList.prototype.append = function (data) {
-  const newNode = new Node(data);
-
-  if (!this.head) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    newNode.prev = this.tail;
-    this.tail.next = newNode;
-    this.tail = newNode;
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  this.length++;
-};
+  append(data) {
+    const newNode = new Node(data);
 
-DoublyLinkedList.prototype.prepend = function (data) {
-  const newNode = new Node(data);
-
-  if (!this.head) {
-    this.head = newNode;
-    this.tail = newNode;
-  } else {
-    newNode.next = this.head;
-    this.head.prev = newNode;
-    this.head = newNode;
-  }
-
-  this.length++;
-};
-
-DoublyLinkedList.prototype.insertAt = function (index, data) {
-  if (index < 0 || index > this.length) {
-    return null;
-  }
-
-  if (index === 0) {
-    return this.prepend(data);
-  }
-
-  if (index === this.length) {
-    return this.append(data);
-  }
-
-  const newNode = new Node(data);
-
-  let currentNode = this.head;
-
-  for (let i = 0; i < index - 1; i++) {
-    currentNode = currentNode.next;
-  }
-
-  newNode.next = currentNode.next;
-  newNode.prev = currentNode;
-  currentNode.next.prev = newNode;
-  currentNode.next = newNode;
-
-  this.length++;
-};
-
-DoublyLinkedList.prototype.get = function (index) {
-  if (index < 0 || index > this.length) {
-    return null;
-  }
-
-  let currentNode = this.head;
-
-  for (let i = 0; i < index; i++) {
-    currentNode = currentNode.next;
-  }
-
-  return currentNode;
-};
-
-DoublyLinkedList.prototype.remove = function (index) {
-  if (index < 0 || index > this.length) {
-    return null;
-  }
-
-  if (index === 0) {
-    this.head = this.head.next;
-
-    if (this.head) {
-      this.head.prev = null;
+    if (!this.head) {
+      this.head = this.tail = newNode; //newNode = { data: 10, next: null, prev: null }
     } else {
-      this.tail = null;
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-  } else if (index === this.length - 1) {
-    this.tail = this.tail.prev;
 
-    if (this.tail) {
-      this.tail.next = null;
+    this.length++;
+  }
+
+  prepend(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
     } else {
-      this.head = null;
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
     }
-  } else {
+
+    this.length++;
+  }
+
+  insertAt(index, data) {
+    if (index < 0 || this.length < index) return null;
+    if (index === 0) return this.prepend(data);
+    if (index === this.length) return this.append(data);
+
     let currentNode = this.head;
+    const newNode = new Node(data);
 
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next;
+    }
+
+    newNode.next = currentNode.next;
+    newNode.prev = currentNode;
+    currentNode.next.prev = newNode;
+    currentNode.next = newNode;
+
+    this.length++;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let currentNode = this.head;
     for (let i = 0; i < index; i++) {
       currentNode = currentNode.next;
     }
 
-    currentNode.prev.next = currentNode.next;
-    currentNode.next.prev = currentNode.prev;
+    return currentNode;
   }
 
-  this.length--;
-};
+  remove(index) {
+    if (index < 0 || index >= this.length) return null;
 
-DoublyLinkedList.prototype.contains = function (data) {
-  let current = this.head;
+    if (index === 0) {
+      this.head = this.head.next;
+      if (this.head) this.head.prev = null;
+      else this.tail = null;
+    } else if (index === this.length - 1) {
+      this.tail = this.tail.prev;
+      if (this.tail) this.tail.next = null;
+      else this.head = null;
+    } else {
+      let currentNode = this.head;
 
-  while (current) {
-    if (current.data === data) {
-      return true;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.next;
+      }
+
+      // A <-> B <-> C <-> D
+      currentNode.prev.next = currentNode.next; //This sets B’s next to point to D instead of C.
+      currentNode.next.prev = currentNode.prev; //This sets D’s prev to point to B instead of C.
     }
-    current = current.next;
   }
 
-  return false;
-};
+  contains(data) {
+    let current = this.head;
 
-DoublyLinkedList.prototype.printAll = function () {
-  let current = this.head;
+    while (current) {
+      if (current.data === data) return true;
+      current = current.next;
+    }
 
-  while (current) {
-    console.log(current.data);
-    current = current.next;
+    return false;
   }
-};
+
+  printAll() {
+    let current = this.head;
+    while (current) {
+      console.log("Data: ", current.data);
+      current = current.next;
+    }
+  }
+}
 
 module.exports = DoublyLinkedList;

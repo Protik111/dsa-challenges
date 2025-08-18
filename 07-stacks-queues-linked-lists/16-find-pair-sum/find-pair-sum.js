99e3c271d220697e7660d208d9473c90ff710075
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-08-18 10:58:32 +0600

    feat: find pair some with doubly linked list

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,22 @@
-const DoublyLinkedList = require('./DoublyLinkedList');
+const DoublyLinkedList = require("./DoublyLinkedList");
 
-function findPairSum() {}
+function findPairSum(arr, target) {
+  const dll = new DoublyLinkedList();
+
+  for (let num of arr) {
+    let current = dll.head;
+
+    while (current) {
+      if (current.data + num === target) {
+        return [current.data, num];
+      }
+      current = current.next;
+    }
+
+    dll.append(num);
+  }
+
+  return null;
+}
 
 module.exports = findPairSum;

dfcbf2e31cd715b113cb7746b21565bc0d794ffd
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-09 11:45:19 +0600

    feat: max depth with bt recursion

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,27 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function maxDepth() {}
+//      a
+//     / \
+//    b   c
+//   / \
+//  d   e
+
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+
+  const leftNode = maxDepth(root.left);
+  const rightNode = maxRight(root.right);
+
+  return Math.max(leftNode, rightNode) + 1;
+}
 
 module.exports = {
   maxDepth,

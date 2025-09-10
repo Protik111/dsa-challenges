3ff2c62af58f09746b63e686da12d906fb166c04
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-10 12:44:58 +0600

    feat: max depth with bfs

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -23,6 +23,31 @@ function maxDepth(root) {
   return Math.max(leftNode, rightNode) + 1;
 }
 
+//with BFS
+function maxDepthBFS(root) {
+  if (!root) {
+    return 0;
+  }
+
+  const depth = 0;
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const current = queue.shift();
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.right(current.right);
+    }
+
+    depth++;
+  }
+
+  return depth;
+}
+
 module.exports = {
   maxDepth,
   Node,

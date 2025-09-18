686da90652dcfc2cfe0370d5ccac62236602b5c0
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-18 13:33:57 +0600

    feat: bst insert method

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
@@ -1,5 +1,43 @@
-class Node {}
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
 
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+    } else {
+      const currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = node;
+            return this;
+          }
+          //If there’s already a node at .left, you move down:
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = node;
+            return this;
+          }
+          //If .right is occupied, you move down:
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+}
 
 module.exports = { Node, BinarySearchTree };

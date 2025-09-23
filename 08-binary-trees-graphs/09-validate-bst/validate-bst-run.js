6e61f11050a28d4bfe91297a22f8fbcd84fc7ac1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-23 13:13:16 +0600

    feat: valid bst tree make

--- 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
@@ -1,4 +1,4 @@
-const { Node, isValidBST } = require('./validate-bst');
+const { Node, isValidBST } = require("./validate-bst");
 
 // Create the binary tree:
 //       8
@@ -7,3 +7,15 @@ const { Node, isValidBST } = require('./validate-bst');
 //    / \
 //   2   6
 
+const root = new Node(8);
+const nodeA = new Node(4);
+const nodeB = new Node(10);
+const nodeC = new Node(2);
+const nodeD = new Node(6);
+
+root.left = nodeA;
+root.right = nodeB;
+nodeA.left = nodeC;
+nodeA.right = nodeD;
+
+console.log(isValidBST(root));

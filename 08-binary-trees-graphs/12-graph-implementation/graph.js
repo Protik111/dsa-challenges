3f975af2151f34a604c16bb78e3defca7c4f9be7
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-28 13:06:24 +0600

    feat: graph removeEdge  method

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -7,9 +7,20 @@ class Graph {
     this.adjacencyList[vertex] = [];
   }
 
-  //   addEdge(vertex1, vertex2) {
-  //     this.adjacencyList[vertex1] =
-  //   }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
 
 // Example Adjacency List

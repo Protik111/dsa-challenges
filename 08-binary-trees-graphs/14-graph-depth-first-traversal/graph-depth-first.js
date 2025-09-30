7aba48b4586e7235e7f312f195c77e14c0555cb3
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-09-30 18:10:18 +0600

    feat: [not done] graph dfs

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,23 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, startingVertex) {
+  if (!graph.adjacencyList[startingVertex]) {
+    return [];
+  }
+
+  let visited = {};
+
+  let stack = new Stack();
+
+  let result = [];
+
+  stack.push(startingVertex);
+
+  visited[startingVertex] = true;
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+  }
+}
 
 module.exports = depthFirstTraversal;

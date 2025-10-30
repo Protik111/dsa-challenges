5dd715b7e3c2bddef4615b09b6d4942a8da24aa4
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-10-30 15:04:00 +0600

    feat: graph BFS

--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,28 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(graph, startingVertex) {
+  if (!graph.adjacencyList[startingVertex]) {
+    return [];
+  }
+
+  const visited = new Set();
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(startingVertex);
+  visited.add(startingVertex);
+
+  while (!queue.isEmpty) {
+    const currentVertex = queue.dequeue();
+    result.push(currentVertex);
+
+    for (const neighbor of graph.adjacencyList[currentVertex]) {
+      if (!visited.has(neighbor)) {
+        queue.enqueue(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+}
 
 module.exports = breadthFirstTraversal;

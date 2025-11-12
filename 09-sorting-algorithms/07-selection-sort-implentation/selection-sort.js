bcba1685f5896f9dc51857a577e828e87c4b2826
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-11-12 10:15:07 +0600

    feat: selection sort solution

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,21 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    //finding the index of the lowest (smallest) element
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    //checking if the smallest index is in the current spot, then swap
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
 
 module.exports = selectionSort;

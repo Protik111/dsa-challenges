59cd61e079d25361f4f4a8848e720bb73dc3591e
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-11-16 13:37:21 +0600

    feat: quick sort with Median-of-three

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -5,6 +5,10 @@ function quickSort(arr) {
 
   //Median-of-three pivot is best practice
   //Use random pivot or median-of-three, because interviewers care about:
+  // const first = arr[0];
+  // const middle = arr[Math.floor(arr.length / 2)];
+  // const last = arr[arr.length - 1];
+  // const pivot = [first, middle, last].sort((a, b) => a - b)[1];
 
   //this is last element pivot
   const pivot = arr[arr.length - 1];

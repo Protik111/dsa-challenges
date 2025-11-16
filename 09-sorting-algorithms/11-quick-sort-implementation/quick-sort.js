493daed1fa97efd23e1a9c38cae338a3757378e7
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-11-16 13:36:18 +0600

    feat: quick sort

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,26 @@
-function quickSort() {
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
+  //Median-of-three pivot is best practice
+  //Use random pivot or median-of-three, because interviewers care about:
+
+  //this is last element pivot
+  const pivot = arr[arr.length - 1];
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 module.exports = quickSort;
